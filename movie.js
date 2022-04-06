3605929f5749c463c4318b571a299245c95c3d51
Edon Abdullahu <edonabdullahu2@gmail.com>
2022-04-06 15:39:02 +0200

    updated

--- movie.js
+++ movie.js
@@ -4,6 +4,11 @@ const m2 = document.querySelector('.movie-2');
 const m3 = document.querySelector('.movie-3');
 const m4 = document.querySelector('.movie-4');
 const m5 = document.querySelector('.movie-5');
+const m6 = document.querySelector('.movie-6');
+const m7 = document.querySelector('.movie-7');
+const m8 = document.querySelector('.movie-8');
+const m9 = document.querySelector('.movie-9');
+const m10 = document.querySelector('.movie-10');
 
 
 
@@ -31,6 +36,26 @@ document.querySelector('.m-5').addEventListener('click', () => {
     m5.classList.remove('hidden');
     removed();
 })
+document.querySelector('.m-6').addEventListener('click', () => {
+    m6.classList.remove('hidden');
+    removed();
+})
+document.querySelector('.m-7').addEventListener('click', () => {
+    m7.classList.remove('hidden');
+    removed();
+})
+document.querySelector('.m-8').addEventListener('click', () => {
+    m8.classList.remove('hidden');
+    removed();
+})
+document.querySelector('.m-9').addEventListener('click', () => {
+    m9.classList.remove('hidden');
+    removed();
+})
+document.querySelector('.m-10').addEventListener('click', () => {
+    m10.classList.remove('hidden');
+    removed();
+})
 
 
 
@@ -40,12 +65,17 @@ overlay.addEventListener('click', () => {
     m3.classList.add('hidden');
     m4.classList.add('hidden');
     m5.classList.add('hidden');
+    m6.classList.add('hidden');
+    m7.classList.add('hidden');
+    m8.classList.add('hidden');
+    m9.classList.add('hidden');
+    m10.classList.add('hidden');
     overlay.classList.add('hidden');
 })
 
 
 document.querySelector('.pickRandom').addEventListener('click', () => {
-    let no = Math.trunc(Math.random() * 5) + 1;
+    let no = Math.trunc(Math.random() * 10) + 1;
     let movieRandom = `.m${no}`;
     console.log(movieRandom);
     document.querySelector(`.movie-${no}`).classList.remove('hidden');
